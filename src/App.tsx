b10100fbc7ff009425be5fb7325caa2c6339319e
ana <124842977+anajulidorio@users.noreply.github.com>
2025-06-03 20:43:52 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 import './App.css';
 import CadastroProcesso from './CadastroProcesso';
+import CadastroCidadao from './CadastroCidadao';
 
 function App() {
   return (
     <div className="App">
+      <h1>Fiscallis</h1>
+
+      <CadastroCidadao />
       <CadastroProcesso />
     </div>
   );
