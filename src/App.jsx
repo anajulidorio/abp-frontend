8917d25fc7ce4f7c6f35301d29ba968f61b07c1c
ana <124842977+anajulidorio@users.noreply.github.com>
2025-06-08 20:47:55 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,26 @@
-import './App.css';
-import CadastroProcesso from './routes/CadastroProcesso';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+import RootLayout from './layouts/RootLayout';
+import PaginaInicial from './routes/PaginaInicial';
+import Login from './routes/Login'
 import CadastroCidadao from './routes/CadastroCidadao';
+import CadastroProcesso from './routes/CadastroProcesso';
+import Consulta from './routes/Consulta';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Fiscallis</h1>
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <RootLayout />,
+    children: [
+      { path: '/', element: <PaginaInicial /> },
+      { path: 'login', element: <Login /> },
+      { path: 'cadastro-cidadao', element: <CadastroCidadao /> },
+      { path: 'cadastro-processo', element: <CadastroProcesso /> },
+      { path: 'consulta', element: <Consulta /> },
+    ],
+  },
+]);
 
-      <CadastroCidadao />
-      <CadastroProcesso />
-    </div>
-  );
+export default function App() {
+  return <RouterProvider router={router} />;
 }
-
-export default App;
