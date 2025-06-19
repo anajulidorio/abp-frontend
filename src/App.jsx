c5c72afc14cd0a5845a98965f143a54de8b14fbd
Lara Dondossola <164415701+LaraDondossola@users.noreply.github.com>
2025-06-19 11:35:27 -0300

    Update App.jsx
    alterações para o login aparecer na primeira tela e depois não ser mostrado novamente

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,44 @@
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { useState } from 'react';
+import { createBrowserRouter, RouterProvider, Navigate } from 'react-router-dom'; 
 
 import RootLayout from './layouts/RootLayout';
 import PaginaInicial from './routes/PaginaInicial';
-import Login from './routes/Login'
+import Login from './routes/Login';
 import CadastroCidadao from './routes/CadastroCidadao';
 import CadastroProcesso from './routes/CadastroProcesso';
 import Consulta from './routes/Consulta';
 
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <RootLayout />,
-    children: [
-      { path: '/', element: <PaginaInicial /> },
-      { path: 'login', element: <Login /> },
-      { path: 'cadastro-cidadao', element: <CadastroCidadao /> },
-      { path: 'cadastro-processo', element: <CadastroProcesso /> },
-      { path: 'consulta', element: <Consulta /> },
-    ],
-  },
-]);
-
 export default function App() {
-  return <RouterProvider router={router} />;
+  const [mostrarLogin, setMostrarLogin] = useState(true);
+
+  const aoAvancar = () => {
+    setMostrarLogin(false);
+  };
+
+  const roteador = createBrowserRouter([
+    {
+      path: '/',
+      element: <RootLayout />,
+      children: [
+        { path: '/', element: <PaginaInicial /> },
+        {
+          path: 'login',
+          element: mostrarLogin ? (
+            <Login aoAvancar={aoAvancar} />
+          ) : (
+            <Navigate to="/" replace />
+          ),
+        },
+        { path: 'cadastro-cidadao', element: <CadastroCidadao /> },
+        { path: 'cadastro-processo', element: <CadastroProcesso /> },
+        { path: 'consulta', element: <Consulta /> },
+      ],
+    },
+  ]);
+
+  if (mostrarLogin) {
+    return <Login aoAvancar={aoAvancar} />;
+  }
+
+  return <RouterProvider router={roteador} />;
 }
